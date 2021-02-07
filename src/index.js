a49fed36f8d9bd8f747cc6d63f55ad4cb1914667
VladUgarov <69145359+VladUgarov@users.noreply.github.com>
2021-02-07 17:54:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  reverseNumber=n=>(n<0?-1:1)*parseInt(n.toString().split('').reverse().join
+  if n(>=0) {
+      return 1* n*parseInt(n.toString().split('').reverse().join
+  }
+  else{
+     return -1* n*parseInt(n.toString().split('').reverse().join         
+  }                                                      
 }
