cd81bd46aaab6d740259b93c319f5c96f84e60fb
VladUgarov <69145359+VladUgarov@users.noreply.github.com>
2021-02-07 18:26:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 module.exports = function reverse (n) {
     let reverse = n.toString().split("").reverse().join("")
-  if (n>=0) {
-      reverse = parseInt(reverse,10)
-      return reverse
-  }
+    reverse = parseInt(reverse, 10)
+    return reverse
 }
